ec2b84aed69bb1fe23aa434be1004bbe6baba289
Aeron7 Inc <hello@aeron7.com>
2020-04-26 21:21:16 +0530

    Learndash 3.0 compatibility

--- learndash.js
+++ learndash.js
@@ -1,3 +1,11 @@
-// Custom CSS for Learndash Compatibility
+// Custom JS for Learndash Compatibility
 // Created by Dexter
 // Aeron7 Inc
+
+
+// lesson Page Image Issue
+if (/lessons/.test(window.location.href)) {
+content_html=document.getElementsByClassName("ld-focus-content")[0].innerHTML;
+new_content_html = '<section class="post-content">' + content_html + '</section>';
+document.getElementsByClassName("ld-focus-content")[0].innerHTML = new_content_html;
+}
