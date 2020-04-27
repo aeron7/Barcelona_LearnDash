f5e8b07d75be1b93c3386e52cb438baf2e0a3026
Aeron7 Inc <hello@aeron7.com>
2020-04-27 19:10:42 +0530

    Topic Image Fix

--- learndash.js
+++ learndash.js
@@ -4,8 +4,14 @@
 
 
 // lesson Page Image Issue
-if (/lessons/.test(window.location.href)) {
+if (/(lessons|topic)/.test(window.location.href)) {
 content_html=document.getElementsByClassName("ld-focus-content")[0].innerHTML;
 new_content_html = '<section class="post-content">' + content_html + '</section>';
 document.getElementsByClassName("ld-focus-content")[0].innerHTML = new_content_html;
+}
+
+if (/courses/.test(window.location.href)) {
+var expandCSS = document.createElement('style')
+expandCSS.innerHTML = ".learndash-wrapper .ld-expand-button.ld-button-alternate .ld-text {background-color: white !important;}";
+document.body.appendChild(expandCSS);
 }
