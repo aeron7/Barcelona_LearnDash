88b79d35eb1ff562c80eb031eefd2ce2d3928447
Aeron7 Inc <hello@aeron7.com>
2020-05-16 15:23:18 +0530

    Update learndash.js

--- learndash.js
+++ learndash.js
@@ -4,13 +4,12 @@
 
 
 // lesson Page Image Issue
-if (/(lessons|topic)/.test(window.location.href)) {
+
+if (/(lessons|topic|quizzes)/.test(window.location.href)) {
 content_html=document.getElementsByClassName("ld-focus-content")[0].innerHTML;
 new_content_html = '<section class="post-content">' + content_html + '</section>';
 document.getElementsByClassName("ld-focus-content")[0].innerHTML = new_content_html;
-}
-
-if (/courses/.test(window.location.href)) {
+} else {
 var expandCSS = document.createElement('style')
 expandCSS.innerHTML = ".learndash-wrapper .ld-expand-button.ld-button-alternate .ld-text {background-color: white !important;}";
 document.body.appendChild(expandCSS);
